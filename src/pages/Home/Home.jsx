43d59f1260ebf9c09a5be4e334d65bd73ad31f6e
RishabhMunakhiya <155822282+RishabhMunakhiya@users.noreply.github.com>
2025-05-31 01:47:55 +0530

    Display coin price using API

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './Home.css'
 const Home = () => {
+
+const {allCoin, currency} =useContext(CoinContext);
+const [displayCoin , setDisplayCoin ] = useState([]);
   return (
-    <div>
-      <h2>Home</h2>
+    <div className='home'>
+      <div className='hero'>
+        <h1>Largest <br/> Crypto Marketplace</h1>
+        <p>Welcome to the world's largest cryptocurrency marketplace. 
+          Sign up to explore more about cryptos.</p>
+          <form>
+            <input type="text" placeholder='Search crypto..' />
+            <button type='submit'>Search</button>
+          </form>
+      </div>
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p style={{textAlign :"center"}} >24H Change</p>
+          <p className='market-cap' >Market Cap</p>
+        </div>
+      </div>
     </div>
   )
 }
