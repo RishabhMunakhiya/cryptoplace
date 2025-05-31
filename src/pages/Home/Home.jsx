6312da01e6d708857c356086b68e3549672bae89
RishabhMunakhiya <155822282+RishabhMunakhiya@users.noreply.github.com>
2025-05-31 15:20:00 +0530

    Create coin details page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react'
 import './Home.css'
 import { CoinContext } from './../../context/CoinContext';
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
 
@@ -56,7 +58,7 @@ useEffect(()=>{
         </div>
         {
           displayCoin.slice(0,10).map((item,index)=>(
-            <div className="table-layout" key={index}>
+            <Link to={`/coin/${item.id}`} className="table-layout" key={index}>
               <p>{item.market_cap_rank}</p>
               <div>
                 <img src={item.image} alt="" />
@@ -68,7 +70,7 @@ useEffect(()=>{
                   {Math.floor(item.price_change_percentage_24h*100)/100}
                 </p>
               <p className='market-cap'>{currency.symbol} {item.market_cap.toLocaleString()}</p>
-            </div>
+            </Link>
           ))
         }
 
